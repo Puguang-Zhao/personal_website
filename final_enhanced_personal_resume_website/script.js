22aa75d769a6cab73dd2938c03f82c26a62fcada
Puguang-Zhao <1124299376@qq.com>
2024-10-26 21:11:22 +0800

    Add Markdown CMS for content management

--- final_enhanced_personal_resume_website/script.js
+++ final_enhanced_personal_resume_website/script.js
@@ -42,13 +42,3 @@ async function loadMarkdown() {
 
 // 初始化
 loadMarkdown();
-
-// 滚动时修改导航栏背景色
-window.addEventListener('scroll', function() {
-    const header = document.querySelector('header');
-    if (window.scrollY > 50) {
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    }
-});
